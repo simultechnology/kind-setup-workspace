c5761ae6abf6ab3f5c656ccd84a233635fc3e7ec
t_ishikawa <tissi0708@gmail.com>
2022-05-17 12:26:57 +0900

    Initial commit

---
+++ assets/kind/kind-v4-with-registry.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -o errexit
+
+# create registry container unless it already exists
+reg_name='host.docker.internal'
+reg_port='5000'
+running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+if [ "${running}" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+# create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --name=$1 --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."${reg_name}:${reg_port}"]
+    endpoint = ["http://${reg_name}:${reg_port}"]
+nodes:
+  - role: control-plane
+  - role: worker
+EOF
+
+set +e
+# connect the registry to the cluster network
+docker network connect "kind" "${reg_name}"
+set -e
+
+# tell https://tilt.dev to use the registry
+# https://docs.tilt.dev/choosing_clusters.html#discovering-the-registry
+for node in $(kind get nodes); do
+  kubectl annotate node "${node}" "kind.x-k8s.io/registry=localhost:${reg_port}";
+done
